93f42e590ec3068c5af78ed367b8a7293ba507c2
Julius Alvarado <nbr1ninrsan2@gmail.com>
2017-03-28 11:22:57 -0700

    initial commit :)

---
+++ README.md
@@ -0,0 +1,6 @@
+## Solving popular algorithms with NodeJS
+  
+Just building stuff. 
+___
+
+[Julius3D.com](http://www.julius3d.com)
---
+++ common_algorithms/common.algorithms1.js
@@ -0,0 +1,17 @@
+/**
+ * Created by julius on 3/28/2017.
+ */
+
+module.exports = function () {
+    console.log('common.algorithms1 says ello ^__^/');
+};
+
+
+function jPalindrome(str) {
+
+}
+
+
+//
+
+
