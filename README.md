7d56eed7b037e57dd2bbd1be66110e3c57205235
Julius Alvarado <nbr1ninrsan2@gmail.com>
2017-03-28 12:07:59 -0700

    palindrome solved

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## Solving popular algorithms with NodeJS
   
-Just building stuff. 
+Just building stuff. Using nodemon to see live updates to the console.
+Won't be using a browser for now, but Eventually I'll code a web UI. 
+Using a browser cost a LOT of RAM for my local machine.
 ___
 
 [Julius3D.com](http://www.julius3d.com)
--- common_algorithms/common.algorithms1.js
+++ common_algorithms/common.algorithms1.js
@@ -3,12 +3,37 @@
  */
 
 module.exports = function () {
-    console.log('common.algorithms1 says ello ^__^/');
+    // regExPrac1();
+    var t1 = jPalindrome('pop'),
+    t2 = jPalindrome('romulus');
+    //jPalindrome('race car');
+    //jPalindrome('afdklj324faffklj90');
+    console.log(t1);
+    console.log(t2);
+
 };
 
 
 function jPalindrome(str) {
+    return str.toLowerCase().replace (/[^a-z0-9]/gi, "") ===
+            str.split("").reverse().join("");
+}
 
+// brushing up on my Regular Expression skills
+function regExPrac1() {
+    var str1 = 'RomulusJuliusAugustusoOptimus';
+    var pat1 = /julius/i;
+    str1 = str1.replace(pat1, 'Vitalius');
+    var search1 = str1.search(pat1); // cache search1
+
+    var regTest = /US/gi.test(str1);
+    var regExec = /OPT/gi.exec(str1);
+
+    // just to see some output:
+    console.log('---');
+    console.log('regExec = '+regExec);
+    console.log("pos of Julius = "+ (search1 === -1 ?
+            'Sorry. pattern "Julius" not found :(' : search1));
 }
 
 
